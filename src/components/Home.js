c6decd5c8a247c2167822578aba3a355124e4ffc
haeina <leehaein0164@gmail.com>
2021-02-24 20:27:32 +0900

    feed contents

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Home.css";
 import {Image} from "react-bootstrap"
 import image from "../image/image.jpeg"
+import {Heart,ChatDots,Envelope,BookmarkCheck} from "react-bootstrap-icons"
 const Home =()=>{
     return (
         <div className = "home">
@@ -13,7 +14,20 @@ const Home =()=>{
             <div className = "follow">follow</div>
             <div className = "write">write</div>
         </div>
-        <div className = "feed">feed</div>
+        <div className = "feed">
+            <div className="header">
+                <img className ="headerProfile" src={image}></img>
+                <div className="headerName">haeina</div>
+            </div>
+            <img className="photo" src = {image}></img>
+            <div className="contents">
+                <Heart/>
+                <ChatDots/>
+                <Envelope/>
+                <BookmarkCheck/>
+            </div>
+            <div className= "comment"></div>
+        </div>
         </div>
     )
 }
