39493a628af0f908c492a19a4985f830ec0e9372
haeina <leehaein0164@gmail.com>
2021-02-24 20:43:03 +0900

    instagram 로고 가져옴

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import './nav.css'
 import {HouseDoorFill,Envelope,Compass,Heart,PersonCircle} from 'react-bootstrap-icons'
+import logo from "../../image/instagram_logo_free.png"
 const Navigation =()=>{
     return (
         <div className ="hi">
-            <div className = "logo"> instagram </div>
+            <div className = "logo"> 
+                <img src={logo}/>
+            </div>
             <div className = "search">검색</div>
             <div className = "icons">
                  <HouseDoorFill className="icon"/>
